9467d7107e8636fa471d5d30c909a15f05131a5c
martin241125 <martincitooo21@gmail.com>
2023-12-18 12:22:59 -0300

    inputs

---
+++ input/inputs.py
@@ -0,0 +1,12 @@
+# pedirle un dato al usuario
+
+#nombre = input('podria darme su nombre? : ')
+
+#mostrando el nombre dado 
+#print(f'que tal {nombre}')
+
+numero = input('faciliteme un numero, voy a multiplicarlo por 2 : ')
+
+numero_facilitado = int(numero) * 2
+
+print(numero_facilitado)
