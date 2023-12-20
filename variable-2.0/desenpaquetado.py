9c9207f7f8ae17810cfe370c5eaf07c0d6f0dc59
martin241125 <martincitooo21@gmail.com>
2023-12-20 11:14:11 -0300

    desmpaquetado de variables

---
+++ variable-2.0/desenpaquetado.py
@@ -0,0 +1,21 @@
+#datos en tupla
+
+datos = (
+    'Martin',
+    'Miño'
+)
+
+nombre, apellido = datos
+
+#datos en litas
+
+datos_list = [
+    'Martin',
+    'Miño',
+    True,
+    32
+]
+
+nombre, apellido, casado, edad = datos_list
+print(nombre)
+print(casado, edad)
