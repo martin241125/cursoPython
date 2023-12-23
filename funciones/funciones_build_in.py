ccc91478942d8b4cd6af0698a1617baf0c148575
martin241125 <martincitooo21@gmail.com>
2023-12-23 15:04:22 -0300

    funciones

---
+++ funciones/funciones_build_in.py
@@ -0,0 +1,22 @@
+#encontrando el numero mayor de una lista
+
+numeros = [23, 22, 44, 55]
+
+numero_mas_alto = max(numeros)
+
+print(numero_mas_alto)
+
+#encontrando el numero menor de una lista
+
+numeros = [23, 22, 44, 55]
+
+numero_mas_bajo = min(numeros)
+
+print(numero_mas_bajo)
+
+#funcion de round para redondear a 6 decimales
+
+numero = round(15.33554,3)
+
+print(numero)
+
